434461a8785a5b17474bd8c56b21d064d2bd7f60
Kishore-L13 <kishore.krrish13@gmail.com>
2026-04-19 13:47:10 +0530

    Git_user

--- git-user/src/Searchbar.jsx
+++ git-user/src/Searchbar.jsx
@@ -1,10 +1,41 @@
 import React,{useState} from "react";
 const Searchbar = ()=>{
-    const [input,setInput] = useState("")
+    const [input, setInput] = useState("");
+const [user, setUser] = useState(null);
+const [loading, setLoading] = useState(false);
+const [error, setError] = useState(null);
+    const fetchUser = async () => {
+  try {
+    setLoading(true);
+    setError(null);
+
+    const res = await fetch(`https://api.github.com/users/${input}`);
+
+    if (!res.ok) throw new Error("User not found");
+
+    const data = await res.json();
+    setUser(data);
+  } catch (err) {
+    setError(err.message);
+  } finally {
+    setLoading(false);
+  }
+};
     return(
         <>
-        <input type="Text" onChange={(e)=>e.target.value} />
-        <button>Search</button>
+    <input value={input} onChange={(e)=>setInput(e.target.value)} />
+    <button onClick={fetchUser}>Search</button>
+        {loading && <p>Loading...</p>}
+{error && <p>{error}</p>}
+
+{user && (
+  <div>
+    <img src={user.avatar_url} width="100" />
+    <h2>{user.name}</h2>
+    <p>Repos: {user.public_repos}</p>
+  </div>
+)}
+   
         </>
     )
 }
